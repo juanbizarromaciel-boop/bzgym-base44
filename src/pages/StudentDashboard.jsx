5ee0000479693e611dcc6482aba6eb50baa5eefa
Juan Bizarro maciel <juanbizarromaciel@gmail.com>
2026-03-14 04:13:14 +0000

    Manual code change

--- src/pages/StudentDashboard.jsx
+++ src/pages/StudentDashboard.jsx
@@ -90,7 +90,7 @@ export default function StudentDashboard() {
     plan.exercises?.forEach(ex => {
       const muscle = ex.exercise_name?.toLowerCase().includes("supino") ? "peito" :
                      ex.exercise_name?.toLowerCase().includes("remada") ? "costas" :
-                     ex.exercise_name?.toLowerCase().includes("Glúteos") ? "gluteo" :
+                     ex.exercise_name?.toLowerCase().includes("bulgaro") ? "gluteo" :
                      ex.exercise_name?.toLowerCase().includes("agachamento") ? "pernas" :
                      ex.exercise_name?.toLowerCase().includes("rosca") ? "biceps" :
                      ex.exercise_name?.toLowerCase().includes("tríceps") ? "triceps" : "outro";
