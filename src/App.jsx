67ac5c0095a6f5cb93e74c03d45cf0f92dc04388
Juan Bizarro maciel <juanbizarromaciel@gmail.com>
2026-05-15 01:53:38 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Toaster } from "@/components/ui/toaster"
+import { Toaster as Sonner } from "sonner"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { pagesConfig } from './pages.config'
@@ -171,6 +172,7 @@ function App() {
           <AuthenticatedApp />
         </Router>
         <Toaster />
+        <Sonner richColors position="top-right" />
       </QueryClientProvider>
     </AuthProvider>
   )
